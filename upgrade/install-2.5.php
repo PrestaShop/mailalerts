e5f11bdb9e1809e67c7a0a69465c1fd48a312006
gRoussac <gregory.roussac@prestashop.com>
2014-04-10 17:02:35 +0200

    [-] MO : Mailalerts new module version

--- upgrade/install-2.5.php
+++ upgrade/install-2.5.php
@@ -5,10 +5,15 @@ if (!defined('_PS_VERSION_'))
 
 function upgrade_module_2_5($object)
 {
-	return Db::getInstance()->execute('
+	Db::getInstance()->execute('
 		ALTER TABLE `'._DB_PREFIX_.'mailalert_customer_oos` 
-		ADD `id_lang` INT( 10 ) UNSIGNED NOT NULL , 
+		ADD `id_lang` INT( 10 ) UNSIGNED NOT NULL 
+	');
+
+	Db::getInstance()->execute('
+		ALTER TABLE `'._DB_PREFIX_.'mailalert_customer_oos` 
 		DROP PRIMARY KEY , 
 		ADD PRIMARY KEY (`id_customer` , `customer_email` , `id_product` , `id_product_attribute` , `id_shop`)
 	');
+	return true;
 }
