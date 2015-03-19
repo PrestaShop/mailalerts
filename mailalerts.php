a19855f98e220e0707881632bf7aed2960d1a537
Gregory Roussac <gregory.roussac@prestashop.com>
2015-03-19 10:46:10 +0100

    // cast assign

--- mailalerts.php
+++ mailalerts.php
@@ -385,7 +385,7 @@ class MailAlerts extends Module
 				),
 			'{total_wrapping}' => Tools::displayPrice($order->total_wrapping, $currency),
 			'{currency}' => $currency->sign,
-			'{gift}' => $order->gift ? 'Yes' : 'No',
+			'{gift}' => (bool)$order->gift,
 			'{gift_message}' => $order->gift_message,
 			'{message}' => $message
 		);
